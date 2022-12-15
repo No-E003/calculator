18a7dd322dd2e250fbb66afc1565e0956f88a30c
No-E003 <96266089+No-E003@users.noreply.github.com>
2022-12-15 11:45:31 -0800

    Created simple functions that will be incorporated in the calculator to find sum

---
+++ script.js
@@ -0,0 +1,25 @@
+//addition func
+const add = (a,b) => a + b;
+console.log(add(4,6));
+console.log(add(4,0))
+console.log(add(3.1,6))
+
+//sub function
+const sub = (a,b) => a - b;
+
+console.log(sub(-5,4));
+console.log(sub(4,6));
+console.log(sub(6,5));
+console.log(sub(2.5,1));
+
+//divide func
+const divide = (a,b) => a/b;
+console.log(divide(4,2));
+
+//multiply func
+const multiply = (a,b) => a * b;
+
+console.log(multiply(6,10));
+console.log(multiply(0,10));
+console.log(multiply(6.545,10));
+console.log(multiply(-6,10));
