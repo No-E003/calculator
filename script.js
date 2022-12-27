c0af131735b590d7f7b94c2334507e42946fe1bf
No-E003 <96266089+No-E003@users.noreply.github.com>
2022-12-26 16:23:59 -0800

    Re-doing the whole project.

--- script.js
+++ script.js
@@ -1,45 +1,33 @@
 //addition func
 const add = (a,b) => a + b;
-console.log(add(4,6));
-console.log(add(4,0))
-console.log(add(3.1,6))
+
 
 //sub function
 const sub = (a,b) => a - b;
 
-console.log(sub(-5,4));
-console.log(sub(4,6));
-console.log(sub(6,5));
-console.log(sub(2.5,1));
-
 //divide func
 const divide = (a,b) => a/b;
-console.log(divide(4,2));
 
 //multiply func
 const multiply = (a,b) => a * b;
 
-console.log(multiply(6,10));
-console.log(multiply(0,10));
-console.log(multiply(6.545,10));
-console.log(multiply(-6,10));
 
-//grabs all the buttons
-const buttons = document.querySelectorAll("button");
-const display = document.getElementById("#display");
+// //grabs all the buttons
+// const buttons = document.querySelectorAll("button");
+// const display = document.getElementById("#display");
+// const numbs = document.querySelectorAll("#option");
+// const operator = document.querySelectorAll("#operator");
+// const decimal = document.querySelector("#decimal");
+// const clear = document.querySelector("#clr");
+// const del = document.querySelector("#del");
 
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    input = button.value;
-    document.getElementById("display").innerHTML = input;
-  });
-});
+// //displays button values
+// buttons.forEach((button) => {
+//   button.addEventListener('click', () => {
+//     input = button.value;
+//     document.getElementById("display").innerHTML += input;
+//   });
+// });
 
-//document.getElementById("display").innerHTML = 5 + 6;
-//let btns = document.getElementsByClassName(".option");
-
-
-
-// btns.addEventListener("click", () => {
-//   document.getElementsByClassName(".display").value
-// }) 
+//let currentOperator = "null";
+//let currentOperation = "null"
