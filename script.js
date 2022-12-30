74fd7bce4a3d5f4ddc7ccbc2aeccc3b799478519
No-E003 <96266089+No-E003@users.noreply.github.com>
2022-12-29 20:11:22 -0800

    Fixed html and css. Also gave functionality to buttons when clicked.

--- script.js
+++ script.js
@@ -13,21 +13,24 @@ const multiply = (a,b) => a * b;
 
 
 // //grabs all the buttons
-// const buttons = document.querySelectorAll("button");
-// const display = document.getElementById("#display");
+const buttons = document.querySelectorAll("button");
+const display = document.getElementById("display");
+const display1 = document.getElementById("display1");
+const display2 = document.getElementById("display2");
 // const numbs = document.querySelectorAll("#option");
 // const operator = document.querySelectorAll("#operator");
 // const decimal = document.querySelector("#decimal");
 // const clear = document.querySelector("#clr");
 // const del = document.querySelector("#del");
 
-// //displays button values
-// buttons.forEach((button) => {
-//   button.addEventListener('click', () => {
-//     input = button.value;
-//     document.getElementById("display").innerHTML += input;
-//   });
-// });
+
+//displays button values
+ buttons.forEach((button) => {
+   button.addEventListener('click', () => {
+     input = button.value;
+     display1.innerHTML += input;
+   })
+ });
 
 //let currentOperator = "null";
-//let currentOperation = "null"
+//let currentOperation = "null";
