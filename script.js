3bebdb0c2b238dcead7fb2bd77a4acd6a9b64b61
No-E003 <96266089+No-E003@users.noreply.github.com>
2023-01-08 00:25:23 -0800

    Have everything working except the equal sign that should grab all variables and sum them up

--- script.js
+++ script.js
@@ -34,34 +34,83 @@ function operate(a,b,operator) {
 }
 
 //clear all
-function clear() {
-  operand1 = ""
-  operand2 = ""
+function clearBtn() {
+  currentValue = "";
+  previousValue = "";
+  operator = "";
   display1.innerHTML = "";
   display2.innerHTML = "";
 }
 
 
 // assigned variables for calculator
-const buttons = document.querySelectorAll("button");
-const display1 = document.getElementById("display1");
-const display2 = document.getElementById("display2");
-const operator = document.getElementById("op");
-const result = document.getElementById("equal");
-let operand1;
-let operand2;
+let numbers = document.querySelectorAll("#num");
+let display1 = document.getElementById("display1");
+let display2 = document.getElementById("display2");
+let equal = document.getElementById("equal");
+let decimal = document.getElementById("decimal");
+let operators = document.querySelectorAll("#op");
+
+let result = "";
+let currentValue = "";
+let operator = "";
+let previousValue = "";
 
 //Initializing calculator once a button is pressed
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if(button.id == "clear") {
-      clear();
-    }
-    else{
-      display1.innerHTML += button.value;
-    }
-  })
+numbers.forEach((number) => number.addEventListener("click", function(e) {
+  handleNumber(e.target.textContent)
+  display2.textContent = currentValue;
+}))
+
+operators.forEach((op) => op.addEventListener("click", function(e) {
+  handleOperator(e.target.textContent)
+  display1.textContent = previousValue + " " + operator;
+  display2.textContent = currentValue;
+}))
+
+clear.addEventListener("click", function() {
+  clearBtn();
 })
+
+equal.addEventListener("click", function() {
+  operate(previousValue,currentValue,operator)
+})
+
+function handleNumber(num) {
+  if(currentValue.length <= 5) {
+    currentValue +=num;
+  }
+}
+
+function handleOperator(op) {
+  operator = op;
+  previousValue = currentValue;
+  currentValue = "";
+}
+
+
+
+// buttons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     if(button.id == "clear") {
+//       clear();
+//     }
+//     if(button.id == "num") {
+//       display1.innerHTML += button.value;
+//       operand2 += button.value;
+//     }
+//      if(button.id == "op") {
+//       display2.innerHTML = button.value;
+//       operator = button.value;
+//       operand1 += operand2
+//     }
+//     if(button.id == "num") {
+//       operand2 = "";
+//       operand2 += button.value;
+//     }
+//   })
+// })
 
 //figure out how to store values in seperate variables
 //once you have that, you can plug it in the opertate function and the system should work
+
